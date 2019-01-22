c557d1933a0fc24a1e7650dcfbd0559dbc59dbc7
Roger Gonzalez <rogergonzalez21@gmail.com>
2019-01-22 19:53:44 -0400

    Added first loadDB method: Books

--- src/data/loadDB.js
+++ src/data/loadDB.js
@@ -1,2 +1,56 @@
 /* eslint no-console: off */
-// Data loader
+/* eslint no-eval: off */
+const path = require('path');
+const mongoose = require('mongoose');
+const fs = require('fs');
+const readline = require('readline');
+const Stream = require('stream');
+
+require('dotenv').config({ path: path.join(__dirname, '/../../.env') });
+
+mongoose.connect(process.env.DATABASE, {
+  useNewUrlParser: true,
+  useCreateIndex: true,
+});
+
+mongoose.Promise = global.Promise; // Tell Mongoose to use ES6 promises
+
+const Book = require('../models/Book');
+
+async function loadBooks() {
+  try {
+    await Book.deleteMany({});
+    const stream = fs.createReadStream('./src/data/books.json');
+    const outstream = new Stream();
+    outstream.readable = true;
+    outstream.writable = true;
+
+    const rl = readline.createInterface({
+      input: stream,
+      output: outstream,
+      terminal: false,
+    });
+
+    let buffer = [];
+    rl.on('line', async (line) => {
+      const book = JSON.parse(JSON.stringify(eval(`(${line})`)));
+      if (book.title && book.asin) {
+        buffer.push(book);
+        if (buffer.length === 1000) {
+          await Book.insertMany(buffer);
+          buffer = [];
+        }
+      }
+    });
+
+    rl.on('close', async () => {
+      await Book.insertMany(buffer);
+      console.log('All books done');
+      return process.exit();
+    });
+  } catch (err) {
+    console.log(err);
+  }
+}
+
+loadBooks();
