2295e935ce3a55ccaf69e4d02a5ddd33ca155dd4
Roger Gonzalez <rogergonzalez21@gmail.com>
2019-01-28 16:16:06 -0400

    Added virtual reviews to Books

--- src/models/Book.js
+++ src/models/Book.js
@@ -22,11 +22,19 @@ const bookSchema = new Schema({
   brand: {
     type: String,
     trim: true,
-    require: 'Please supply a brand',
   },
   categories: [[String]],
+}, {
+  toJSON: { virtuals: true },
+  toObject: { virtuals: true },
 });
 
 bookSchema.plugin(mongodbErrorHandler);
+
+bookSchema.virtual('reviews', {
+  ref: 'Review',
+  localField: 'asin',
+  foreignField: 'asin',
+});
 
 module.exports = mongoose.model('Book', bookSchema);
